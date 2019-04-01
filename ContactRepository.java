7452860bb11f5d293850e68ed1743dd8618840c7
0pjg <41444289+0pjg@users.noreply.github.com>
2019-03-31 21:52:01 -0400

    Delete ContactRepository.java

--- ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.props.contact;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.props.contact.Contact;
-
-public interface ContactRepository extends JpaRepository<Contact, String> {
-
-}
